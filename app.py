e9cc06cec9773c4486ba4e80d4d185afd15ebd5a
Shashmithayoganarajah <139777522+Shashmithayoganarajah@users.noreply.github.com>
2023-09-04 12:06:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/team")
+def team():
+    return render_template("blog.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
